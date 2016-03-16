4069dad319f3a3bacf853bb6588bbcbc19a802e4
sammita94 <sammitachakravarti@yahoo.com>
2016-03-16 16:11:33 +0530

    First commit

---
+++ LoginUsingPHP/app/src/main/java/app/AppConfig.java
@@ -0,0 +1,9 @@
+package app;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://172.16.59.26/FoodApp/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://172.16.59.26/FoodApp/register.php";
+}
